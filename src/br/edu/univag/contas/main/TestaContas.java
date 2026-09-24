6aab87d1212553f92c2956117ad078ea9a80b907
ensunivag <ensunivag@outlook.com>
2026-09-24 13:16:43 +0000

    Cap 11 Exercício 3

---
+++ src/br/edu/univag/contas/main/TestaContas.java
@@ -0,0 +1,49 @@
+package br.edu.univag.contas.main;
+
+import br.edu.univag.contas.ManipuladorDeContas;
+import br.edu.univag.contas.ManipuladorDeSeguroDeVida;
+import br.edu.univag.contas.modelo.Conta;
+import br.edu.univag.contas.tela.SistemaBancario;
+
+public class TestaContas {
+    public static void main(String[] args) {
+        criaEListaContas();
+        criaEListaSeguros();
+    }
+
+    private static void criaEListaContas() {
+        ManipuladorDeContas mdc = new ManipuladorDeContas();
+
+        mdc.criaConta("Duke", 1234, "6789-X", "ContaCorrente");
+        Conta conta1 = mdc.getConta();
+        SistemaBancario.adicionarConta(conta1);
+        
+        mdc.deposita(500);
+        mdc.saca(250);
+
+        mdc.criaConta("Pernalonga", 4562, "5524-3", "ContaPoupanca");
+        Conta conta2 = mdc.getConta();
+        SistemaBancario.adicionarConta(conta2);
+        
+        mdc.deposita(300);
+        mdc.saca(100);
+
+        mdc.setConta(conta1);
+        mdc.transfere(conta2, 20);
+        mdc.transfere(conta2, 229.80);
+
+        SistemaBancario.listarContas();
+    }
+
+    private static void criaEListaSeguros() {
+        ManipuladorDeSeguroDeVida mdsv = new ManipuladorDeSeguroDeVida();
+
+        mdsv.criaSeguro(5001, "Patolino", 10_000);
+        SistemaBancario.adicionarSegur(mdsv.getSeguroDeVida());
+
+        mdsv.criaSeguro(5002, "Kara Kent", 18_000);
+        SistemaBancario.adicionarSegur(mdsv.getSeguroDeVida());
+
+        SistemaBancario.listarSeguros();
+    }
+}
--- src/br/edu/univag/contas/tela/SistemaBancario.java
+++ src/br/edu/univag/contas/tela/SistemaBancario.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 import br.edu.univag.contas.modelo.Conta;
+import br.edu.univag.contas.modelo.SeguroDeVida;
 
 public class SistemaBancario {
     private static List<Conta> contas = new ArrayList<>();
+    private static List<SeguroDeVida> seguros = new ArrayList<>();
     
     public static void adicionarConta(Conta conta) {
         contas.add(conta);
     }
     
     public static void listarContas() {
+        System.out.println();
         System.out.printf("%-20s%7s%8s%15s %s%n", "Titular", "Número", "Agência", "Saldo", "Tipo");
 
         if (contas.isEmpty()) {
@@ -22,6 +25,25 @@ public class SistemaBancario {
             for(Conta conta : contas) {
                 System.out.printf("%-20S%7s%8s%15.2f %s%n", conta.getTitular(),
                     conta.getNumero(), conta.getAgencia(), conta.getSaldo(), conta.getTipo());
+            }
+        }
+    }
+
+    public static void adicionarSegur(SeguroDeVida seguro) {
+        seguros.add(seguro);
+    }
+    
+    public static void listarSeguros() {
+        System.out.println();
+        System.out.printf("%-20s%8s%15s %s%n", "Titular", "Apólice", "Valor", "Tipo");
+
+        if (seguros.isEmpty()) {
+            System.out.println("Nenhum seguro cadastrada");
+        }
+        else {
+            for(SeguroDeVida seguro : seguros) {
+                System.out.printf("%-20S%8s%15.2f %s%n", seguro.getTitular(),
+                    seguro.getNumeroApolice(),  seguro.getValor(), seguro.getTipo());
             }
         }
     }
