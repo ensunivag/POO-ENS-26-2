c23f78b346f75558ca2db9d6a49b9b097585d7e2
ensunivag <ensunivag@outlook.com>
2026-09-24 14:49:14 +0000

    Cap 12 Exercício 2.3

--- src/br/edu/univag/contas/main/cap12/TestaDepositoInvalido.java
+++ src/br/edu/univag/contas/main/cap12/TestaDepositoInvalido.java
@@ -5,7 +5,12 @@ import br.edu.univag.contas.modelo.ContaPoupanca;
 
 public class TestaDepositoInvalido {
     public static void main(String[] args) {
+        try {
         Conta conta = new ContaPoupanca();
         conta.depositar(0.0);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
